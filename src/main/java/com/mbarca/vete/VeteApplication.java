1b9c4d452a8d6841183de9148659edcd85b1e4da
Mbarca89 <mauriciobarca1989@gmail.com>
2023-12-26 06:19:27 -0300

    First commit

---
+++ src/main/java/com/mbarca/vete/VeteApplication.java
@@ -0,0 +1,13 @@
+package com.mbarca.vete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbarca/vete/VeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbarca.vete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
